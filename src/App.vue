ba3aba13359d5d4cddd092a50b300c1df0338267
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-09-18 23:56:02 +0100

    getlinked project

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <header>
+      <div class="header-overlay">
+        <img src="@/assets/images/purple-lens-header.svg" alt="Purple Lens Logo" />
+      </div>
+      <div class="logo">
+        <img
+          alt="GetLinked Logo"
+          class="logo"
+          src="@/assets/images/getlinked-logo.svg"
+          width="125"
+          height="125"
+        />
+      </div>
+
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+    <HelloWorld />
+  </div>
+</template>
+
+<style scoped>
+header {
+  background-color: #150e28;
+  position: relative;
+}
+
+.header-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 125px;
+  height: 125px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="bg-red-500">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<style scoped></style>
